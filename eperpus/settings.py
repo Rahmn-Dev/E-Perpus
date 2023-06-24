c004eb098579e53944746adda9455c721e9bdede
Rahman21_ <66931894+Rahmn-Dev@users.noreply.github.com>
2023-06-24 17:07:10 +0700

    Update settings.py
    secret key

--- eperpus/settings.py
+++ eperpus/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x9h==72o$8el18%@oj6kwq8%olam)_go2^r9_y3wgzhpbx595k'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
